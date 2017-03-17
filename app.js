3b6cf8c9cc3a6573e0968b19a7f09bb7a988d3ed
tipirnenichowdary <tipirnenichowdary@gmail.com>
2017-03-17 17:01:43 -0400

    apiCommit

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var app  = angular.module("restaurantApp", ['ngMaterial'])
 
 app.controller("restController", function($scope, $http){
 $scope.search = function(){
-    debugger
-$http.defaults.headers.common.user_key = "4b0d3ef99f469fe811b8d83f950242f6";
+$http.defaults.headers.common["user-key"] = "4b0d3ef99f469fe811b8d83f950242f6";
 $http.get("https://developers.zomato.com/api/v2.1/search?q="+$scope.name)
     .then(function(response) {
-        $scope.zip= response;
+        $scope.results= response;
+        debugger
         console.log("hello");
     });
 };
