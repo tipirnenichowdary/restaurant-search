d72524935ce74395b5ad5b82e7878081d46edd53
tipirnenichowdary <tipirnenichowdary@gmail.com>
2017-03-23 08:33:45 -0400

    api-2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ app.run(['$rootScope', function($rootScope){
     $rootScope.cityIds = {};
 }]);
 app.controller("restController",['$rootScope','$scope', '$http', 'cityFactory','hotelsList', function($rootScope, $scope, $http, cityFactory, hotelsList){
+
+$rootScope.a = hotelsList.getdata(6);
 $scope.value = false;
 $scope.search = function(){
 $http.defaults.headers.common["user-key"] = "4b0d3ef99f469fe811b8d83f950242f6";
@@ -11,6 +13,7 @@ var idList = cityFactory.getCityId($scope.name)
 .then(function(response) {
     var cityIdList =[];
         var results = response.data;
+        $scope.results = results;
         var value;
         if(results.location_suggestions != null && results.location_suggestions.length == 0)
         {
@@ -20,12 +23,16 @@ var idList = cityFactory.getCityId($scope.name)
         {
             for(var obj in results.location_suggestions)
             {
-               cityIdList.push(results.restaurants[obj].restaurant.id);
+               cityIdList.push(results.location_suggestions[obj].country_id);
             }
         }
-        console.log(cityIdList);
-       $scope.finalData =  hotelsList.getdata(cityIdList);
+        console.log(cityIdList)
     });
+$scope.FindHotels = function(id)
+{ 
+$scope.hotList = hotelsList.getdata(id);
+    
+}
 }
 }])
 
@@ -42,15 +49,13 @@ app.factory("cityFactory",['$http',function($http){
 
 app.service("hotelsList",['$http',function($http){
     var i;
-  this.getdata = function(idList){
-        for(i = 0; i<idList.length; i++)
-    {
-        var url = "https://developers.zomato.com/api/v2.1/search?entity_id="+ idList[i];
-    this.hotel = function(){ $http.get(url)
-    .then(function(response) {
+  this.getdata = function(id){ 
+        var url = "https://developers.zomato.com/api/v2.1/search?entity_id="+id+"&entity_type=city";
+        $http.get(url).then(function(response) {
         console.log("hi");
-    })
-}
-    }
+        var i = response;
+       
+        }); 
+    return i; 
   }
 }]);
